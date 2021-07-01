b5529a734e8e494e0961e265902ee156f7377c52
Alexander Sonnie <Sonnie.2@live.com>
2021-07-01 10:41:21 -0400

    Fixed AccountDao and finished JdbcAccountDao

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -10,14 +10,16 @@ public interface AccountDao {
     //findUserById(int userId) -- user id, findAccountById(int Id) -- account id
     Account findAccountById(Long userId);
 
+    Account findUserById(Long userId);
+
     //Has to be in big decimal. getBalance(int userId). get the balance from each user based on their id
     BigDecimal getBalance(Long userId);
 
     //big decimal again. Love it. addToBalance( amount to add???, int id) based off of account id.
-    boolean addToBalance(BigDecimal amount, Long userId);
+    BigDecimal addToBalance(BigDecimal amount, Long userId);
 
     //BIG DECIMAL. subtractFromBalance (amount to subtract??, int id) based off of account id.
-    boolean subtractFromBalance(BigDecimal amount, Long userId);
+    BigDecimal subtractFromBalance(BigDecimal amount, Long userId);
 
     //
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,6 +1,7 @@
 package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Account;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
@@ -22,30 +23,86 @@ public class JdbcAccountDao implements AccountDao {
 
 
     @Override
-    public Account findAccountById(Long user_Id) {
-        return null;
+    public Account findAccountById(Long user_id) {
+        String sqlString = "SELECT * FROM accounts WHERE user_id = ?";
+        Account account = null;
+        try {
+            SqlRowSet result = jdbcTemplate.queryForRowSet(sqlString, user_id);
+            account = mapRowToAccount(result);
+        } catch (DataAccessException e) {
+            System.out.println("Cannot access data");
+        }
+        return account;
     }
 
     @Override
-    public BigDecimal getBalance(Long userId) {
+    public Account findUserById(Long user_id) {
+        String sqlString = "SELECT * FROM accounts WHERE user_id = ?";
+        Account account = null;
+        try {
+            SqlRowSet result = jdbcTemplate.queryForRowSet(sqlString, user_id);
+            account = mapRowToAccount(result);
+        } catch (DataAccessException e) {
+            System.out.println("Cannot access data");
+        }
+        return account;
+    }
+
+
+
+    @Override
+    public BigDecimal getBalance(Long user_id) {
         String sqlString = "SELECT balance FROM accounts WHERE user_id = ?";
         SqlRowSet results = null;
         BigDecimal balance = null;
 
         try {
-            res
+            results = jdbcTemplate.queryForRowSet(sqlString, user_id);
+            if(results.next()) {
+                balance = results.getBigDecimal("balance");
+            }
+        } catch (DataAccessException e) {
+            System.out.println("Cannot access data");
         }
 
-        return null;
+        return balance;
     }
 
     @Override
-    public boolean addToBalance(BigDecimal amount, Long userId) {
-        return false;
+    public BigDecimal addToBalance(BigDecimal amount, Long user_id) {
+       Account account = findAccountById(user_id);
+       BigDecimal updatedBalance = account.getBalance().add(amount); // if BigDecimal cannot be into JSON, switch to double or long.
+        System.out.println(updatedBalance);
+        String sqlString = "UPDATE accounts SET balance = ? WHERE user_id = ?";
+        try {
+            jdbcTemplate.update(sqlString, updatedBalance, user_id);
+        } catch (DataAccessException e) {
+            System.out.println("Cannot access data");
+        }
+        return account.getBalance();
     }
 
     @Override
-    public boolean subtractFromBalance(BigDecimal amount, Long userId) {
-        return false;
+    public BigDecimal subtractFromBalance(BigDecimal amount, Long user_id) {
+        Account account = findAccountById(user_id);
+        BigDecimal updatedBalance = account.getBalance().subtract(amount);
+        String sqlString = "UPDATE accounts SET balance = ? WHERE user_id = ?";
+        try {
+            jdbcTemplate.update(sqlString, updatedBalance, user_id);
+        } catch (DataAccessException e) {
+            System.out.println("Cannot access data");
+        }
+        return account.getBalance();
     }
+
+
+    private Account mapRowToAccount(SqlRowSet result) {
+        Account account = new Account();
+        account.setBalance(result.getBigDecimal("balance"));
+        account.setAccount_id(result.getLong("account_id"));
+        account.setUser_id(result.getLong("user_id"));
+        return account;
+    }
+
 }
+
