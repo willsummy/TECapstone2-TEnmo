713cccfccca2fbdc7ff0ce28d3ef40d6cce36273
Alexander Sonnie <Sonnie.2@live.com>
2021-07-01 10:03:08 -0400

    Working on Override methods for JdbcAccountDao

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,4 +1,46 @@
 package com.techelevator.tenmo.dao;
 
-public class JdbcAccountDao {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+
+@Component
+public class JdbcAccountDao implements AccountDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao() { }
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+
+    @Override
+    public Account findAccountById(Long userId) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getBalance(Long userId) {
+        String sqlString = "SELECT balance FROM accounts WHERE user_id = ?";
+        SqlRowSet results = null;
+        BigDecimal balance = null;
+
+        return null;
+    }
+
+    @Override
+    public boolean addToBalance(BigDecimal amount, Long userId) {
+        return false;
+    }
+
+    @Override
+    public boolean subtractFromBalance(BigDecimal amount, Long userId) {
+        return false;
+    }
 }
