307293314a1f5abb713a0c1dd9abfc7d26facb91
William Summy <willsummy814@gmail.com>
2021-07-01 09:32:14 -0400

    Adding planning notes to AccountDao.java

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,16 +1,21 @@
 package com.techelevator.tenmo.dao;
 
+import java.math.BigDecimal;
+
 public interface AccountDao {
 
     //Find accounts and users.
     //findUserById(int userId) -- user id, findAccountById(int Id) -- account id
-
+    Account findAccountById(Long userId);
 
     //Has to be in big decimal. getBalance(int userId). get the balance from each user based on their id
+    BigDecimal getBalance(Long userId);
 
     //big decimal again. Love it. addToBalance( amount to add???, int id) based off of account id.
+    boolean addToBalance(BigDecimal amount, Long userId);
 
     //BIG DECIMAL. subtractFromBalance (amount to subtract??, int id) based off of account id.
+    boolean subtractFromBalance(BigDecimal amount, Long userId);
 
     //
 }
