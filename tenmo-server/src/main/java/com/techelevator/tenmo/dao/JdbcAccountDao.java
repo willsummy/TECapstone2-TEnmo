8aaab6071aed3f2e44fbc6ea9da9092fd317268e
Alexander Sonnie <Sonnie.2@live.com>
2021-07-05 11:19:16 -0400

    Almost done with finalizing project

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -65,33 +65,6 @@ public class JdbcAccountDao implements AccountDao {
         return balance;
     }
 
-//    @Override
-//    public BigDecimal addToBalance(BigDecimal amount, Long user_id) throws AccountNotFoundException {
-//       Account account = findAccountById(user_id);
-//       BigDecimal updatedBalance = account.getBalance().add(amount); // if BigDecimal cannot be into JSON, switch to double or long.
-//        System.out.println(updatedBalance);
-//        String sqlString = "UPDATE accounts SET balance = ? WHERE user_id = ?";
-//        try {
-//            jdbcTemplate.update(sqlString, updatedBalance, user_id);
-//        } catch (DataAccessException e) {
-//            System.out.println("Cannot access data");
-//        }
-//        return account.getBalance();
-//    }
-//
-//    @Override
-//    public BigDecimal subtractFromBalance(BigDecimal amount, Long user_id) throws AccountNotFoundException {
-//        Account account = findAccountById(user_id);
-//        BigDecimal updatedBalance = account.getBalance().subtract(amount);
-//        String sqlString = "UPDATE accounts SET balance = ? WHERE user_id = ?";
-//        try {
-//            jdbcTemplate.update(sqlString, updatedBalance, user_id);
-//        } catch (DataAccessException e) {
-//            System.out.println("Cannot access data");
-//        }
-//        return account.getBalance();
-//    }
-
     @Override
     public boolean transferFunds(BigDecimal amount, Long sender_account_id, Long receiver_account_id) throws AccountNotFoundException {
 
